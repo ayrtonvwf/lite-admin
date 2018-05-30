1493cd8679ec35545aeed69c2da14dbcc737fe88
Ayrton Fidelis <ayrton.vargas33@gmail.com>
2018-05-30 20:28:34 -0300

    Simplify gulp tasks and update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,31 @@
-'use strict';
+'use strict'
 
-const gulp = require('gulp');
-const watch = require('gulp-watch');
-const pug = require('gulp-pug');
-const scss = require('gulp-sass');
-const prefix = require('gulp-autoprefixer');
-var browserSync = require('browser-sync').create();
+const gulp = require('gulp')
+const watch = require('gulp-watch')
+const pug = require('gulp-pug')
+const scss = require('gulp-sass')
+const prefix = require('gulp-autoprefixer')
 
-const dev = 'src';
-const server = 'dist';
+const dev = 'src'
+const server = 'dist'
+
+const build_html = function() {
+    return gulp.src(dev + '/pug/*.pug')
+        .pipe(pug({pretty: true}))
+        .pipe(gulp.dest(server));
+}
+
+const build_css = function() {
+    return gulp.src(dev+'/scss/*.scss')
+        .pipe(scss())
+        .pipe(prefix())
+        .pipe(gulp.dest(server+'/css'));
+}
 
 gulp.task('watch', function() {
-    watch(dev+'/pug/*.pug', function () {
-        return gulp.src(dev+'/pug/*.pug')
-            .pipe(pug({pretty: true}))
-            .pipe(gulp.dest(server))
-            .pipe(browserSync.stream());
-    });
-
-    watch(dev+'/scss/**/*.scss', function () {
-        return gulp.src(dev+'/scss/*.scss')
-            .pipe(scss())
-            .pipe(prefix())
-            .pipe(gulp.dest(server+'/css'))
-            .pipe(browserSync.stream());
-    });
-
-    browserSync.init({
-        server: {
-            baseDir: server
-        }
-    });
+    watch(dev+'/pug/*.pug', build_html);
+    watch(dev+'/scss/**/*.scss', build_css);
 });
+
+gulp.task('build_html', build_html);
+gulp.task('build_css', build_css);
