f206faa1d160d7044da609c9f5116ddc105043fa
Ayrton Fidelis <ayrton.vargas33@gmail.com>
2018-06-02 11:45:54 -0300

    Compile pug to docs

--- gulpfile.js
+++ gulpfile.js
@@ -7,25 +7,27 @@ const scss = require('gulp-sass')
 const prefix = require('gulp-autoprefixer')
 
 const dev = 'src'
-const server = 'dist'
+const dist = 'dist'
+const docs = 'docs'
 
 const build_html = function() {
     return gulp.src(dev + '/pug/*.pug')
         .pipe(pug({pretty: true}))
-        .pipe(gulp.dest(server));
+        .pipe(gulp.dest(docs))
 }
+
 
 const build_css = function() {
     return gulp.src(dev+'/scss/*.scss')
         .pipe(scss())
         .pipe(prefix())
-        .pipe(gulp.dest(server+'/css'));
+        .pipe(gulp.dest(dist))
 }
 
 gulp.task('watch', function() {
-    // watch(dev+'/pug/*.pug', build_html);
-    watch(dev+'/scss/**/*.scss', build_css);
-});
+    // watch(dev+'/pug/*.pug', build_html)
+    watch(dev+'/scss/**/*.scss', build_css)
+})
 
-gulp.task('build_html', build_html);
-gulp.task('build_css', build_css);
+gulp.task('build_html', build_html)
+gulp.task('build_css', build_css)
