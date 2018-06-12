cf14c0fa7b360d5c7c25e311f6852ca01d9f8095
Ayrton Fidelis <ayrton.vargas33@gmail.com>
2018-06-12 19:47:23 -0300

    Change permission of files

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@
 
 const gulp = require('gulp')
 const watch = require('gulp-watch')
-const pug = require('gulp-pug')
 const scss = require('gulp-sass')
 const prefix = require('gulp-autoprefixer')
 const sourcemaps = require('gulp-sourcemaps')
@@ -10,13 +9,6 @@ const sourcemaps = require('gulp-sourcemaps')
 const dev = './src'
 const dist = './dist'
 const docs = './docs'
-
-const build_html = function() {
-    return gulp.src(dev + '/pug/*.pug')
-        .pipe(pug({pretty: true}))
-        .pipe(gulp.dest(docs))
-}
-
 
 const build_css = function() {
     return gulp.src(dev+'/scss/*.scss')
@@ -29,9 +21,7 @@ const build_css = function() {
 }
 
 gulp.task('watch', function() {
-    // watch(dev+'/pug/*.pug', build_html)
     watch(dev+'/scss/**/*.scss', build_css)
 })
 
-gulp.task('build_html', build_html)
 gulp.task('build_css', build_css)
